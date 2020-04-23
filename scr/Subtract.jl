2c59e89ccfa2178b534146083ca3c74f9ed17a3a
llffyy2971 <46347645+llffyy2971@users.noreply.github.com>
2020-04-23 11:26:20 +0800

    Add files via upload

---
+++ scr/Subtract.jl
@@ -0,0 +1,66 @@
+"""
+This module contains a code example related to
+
+Think Julia, 2nd Edition
+by Fang Liu
+http://
+
+Copyright 2020 Fang Liu
+
+License: http://creativecommons.org/licenses/by/4.0/
+"""
+module Subtract
+
+"""
+    subtract(d1, d2)
+
+Returns a set of all keys that appear in d1 but not d2.
+
+# Arguments
+- 'd1': dictionary.
+- 'd2': dictionary.
+"""
+function subtract(d1, d2)
+    setdiff(keys(d1), keys(d2))
+end
+
+function processfile(filename, skipheaderandends)
+    hist = Dict()
+    file = open(filename)
+
+    skipheaderandends && skipgutenbergheader(file)
+
+    for line in eachline(file)
+        skipheaderandends && startswith(line, "*** END OF THIS") && break
+        processline!(hist, line)
+    end
+    close(file)
+    hist
+end
+
+function skipgutenbergheader(file)
+    for line in eachline(file)
+        startswith(line, "*** START OF THIS") && break
+    end
+end
+    
+function processline!(hist, line)
+    line = replace(line, '-'=>' ')
+    for word in split(line)
+        word = string(filter(isletter, [word...])...)
+        word = lowercase(word)
+        hist[word] = get(hist, word, 0) + 1
+    end
+end
+
+
+hist = processfile("./scr/emma.txt", true);
+words = processfile("./scr/CROSSWD.TXT", false);
+
+diff = subtract(hist, words);
+println("The words in the book that aren't in the word list are:")
+for word in diff
+    print(word, " ")
+end
+
+end
