d98ca10651e2b4401b3e41ea8e4268719ef31d7a
Fang Liu <46347645+fangliu-tju@users.noreply.github.com>
2020-07-01 15:11:25 +0800

    Add files via upload

---
+++ scr/spiral.jl
@@ -0,0 +1,44 @@
+"""
+This file contains a code example related to
+
+Julia 程序设计基础
+by Fang Liu
+https://github.com/fangliu-tju/JuliaBasics/blob/master/scr/spiral.jl
+
+Copyright 2020 Fang Liu
+
+License: http://creativecommons.org/licenses/by/4.0/
+"""
+
+using Luxor
+
+"""
+    draw_spiral(t, n, len=3, a=0.1, b=0.0002)
+
+Draws an Archimedian spiral starting at the origin.    
+
+# Arguments
+- `t::Turtle`: turtle
+- `n`: how many line segments to draw
+- `len`: how long each segment is
+- `a`: how loose the initial spiral starts out (larger is looser)
+- `b`: how loosly coiled the spiral is (larger is looser)
+
+http://www.2dcurves.com/spiral/spiral.html
+"""
+function draw_spiral(t, n, len=3, a=0.1, b=0.0002)
+    
+    theta = 0.0
+
+    for _ in 1:n
+        Forward(t, len)
+        dtheta = 1 / (a + b * theta)
+
+        Turn(t, dtheta)
+        theta += dtheta
+    end
+end
+
+# create the world and bob
+bob = Turtle();
+@svg draw_spiral(bob, 250)
